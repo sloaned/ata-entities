9108eb4af7c643f0a4b2a541ea6dbbace87c67d8
Joshua Tucker <jtucker@catalystitservices.com>
2016-04-05 10:23:47 -0700

    deleted assessmentOption

--- src/test/java/assessment/assessment/assessmentOption.java
+++
@@ -1,9 +0,0 @@
-package assessment.assessment;
-
-/**
- * Created by Alysha Recore on 4/1/2016.
- */
-public enum AssessmentOption {
-
-    VALID_ASSESSMENT, BAD_ASSESSMENT_LONG_NAME, BAD_ASSESSMENT_BAD_NAME, BAD_ASSESSMENT_NULL_NAME, BAD_ASSESSMENT_NULL_VERSION
-}
