18cf4681f7a15e47fb7079477ed38c6e6935f9fd
Joshua Tucker <jtucker@catalystitservices.com>
2016-05-03 18:09:05 -0700

    refactored User Factory

--- src/main/java/assessment/factories/user/UserFactory.java
+++ src/main/java/assessment/factories/user/UserFactory.java
@@ -14,238 +14,106 @@ import java.util.List;
 
 public class UserFactory extends TestBase {
 
+    public User assembleBaseUser() {
+
+		User baseUser = new User();
+
+		baseUser.setVersion(VALID_ENTITY_VERSION);
+		baseUser.setActive(true);
+		baseUser.setAvatar(VALID_GRAVATAR_HASH);
+		baseUser.setEmail(VALID_USER_EMAIL);
+		baseUser.setFirstName(VALID_USER_NAME);
+		baseUser.setLastName(VALID_USER_NAME);
+		baseUser.setProfileDescription(VALID_DESCRIPTION);
+		baseUser.setStartDate(new Date());
+		baseUser.setTitle(VALID_TITLE);
+
+		return baseUser;
+
+    }
+
     public User assembleUser(UserOption userOption) {
 
-        User user = new User();
+        User user = assembleBaseUser();
 
         switch(userOption){
 
             case VALID_ACTIVE_DEVELOPER:
-                user.setVersion(USER_CURRENT_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
+				user.setVersion(USER_CURRENT_VERSION);
                 break;
 
             case VALID_INACTIVE_DEVELOPER:
                 user.setVersion(USER_CURRENT_VERSION);
                 user.setActive(false);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_FIRST_NAME_EMPTY:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
                 user.setFirstName(EMPTY_STRING);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_FIRST_NAME_NULL:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
                 user.setFirstName(null);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_FIRST_NAME_SPECIAL_CHARACTERS:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
                 user.setFirstName(INVALID_USER_NAME_SPECIAL_CHARACTERS);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 user.setVersion(USER_CURRENT_VERSION);
                 break;
 
             case INVALID_USER_FIRST_NAME_LONG:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
                 user.setFirstName(generateRandomString(51));
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 user.setVersion(USER_CURRENT_VERSION);
                 break;
 
             case INVALID_USER_LAST_NAME_EMPTY:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
                 user.setLastName(EMPTY_STRING);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_LAST_NAME_NULL:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
                 user.setLastName(null);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_LAST_NAME_SPECIAL_CHARACTERS:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
                 user.setLastName(INVALID_USER_NAME_SPECIAL_CHARACTERS);
-                user.setStartDate(new Date());
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_LAST_NAME_LONG:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
                 user.setLastName(generateRandomString(51));
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_EMAIL_EMPTY:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
                 user.setEmail(EMPTY_STRING);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_EMAIL_NULL:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
                 user.setEmail(null);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_EMAIL_INVALID_ADDRESS:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
                 user.setEmail(INVALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
                 break;
 
             case INVALID_USER_VERSION_ZERO:
                 user.setVersion(0);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_VERSION_NULL:
                 user.setVersion(null);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_DESCRIPTION_LONG:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
                 user.setProfileDescription(generateRandomString(256));
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_ISACTIVE_NULL:
-                user.setVersion(VALID_ENTITY_VERSION);
                 user.setActive(null);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
-                user.setTitle(VALID_TITLE);
                 break;
 
             case INVALID_USER_TITLE_NULL:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
                 user.setTitle(null);
                 break;
 
             case INVALID_USER_TITLE_EMPTY:
-                user.setVersion(VALID_ENTITY_VERSION);
-                user.setActive(true);
-                user.setAvatar(VALID_GRAVATAR_HASH);
-                user.setEmail(VALID_USER_EMAIL);
-                user.setFirstName(VALID_USER_NAME);
-                user.setLastName(VALID_USER_NAME);
-                user.setProfileDescription(VALID_DESCRIPTION);
-                user.setStartDate(new Date());
                 user.setTitle(EMPTY_STRING);
                 break;
         }
