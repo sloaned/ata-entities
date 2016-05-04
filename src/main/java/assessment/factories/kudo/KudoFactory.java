10dd0bd1c9fd2b0a0a049b6bc5a98bdfd1df31f9
Joshua Tucker <jtucker@catalystitservices.com>
2016-05-04 10:23:54 -0700

    refactored Kudo Factory

--- src/main/java/assessment/factories/kudo/KudoFactory.java
+++ src/main/java/assessment/factories/kudo/KudoFactory.java
@@ -13,83 +13,58 @@ import java.util.Date;
  */
 public class KudoFactory extends TestBase {
 
+    public Kudo assembleBaseKudo() {
+
+		Kudo baseKudo = new Kudo();
+
+		baseKudo.setReviewedId(VALID_REVIEWED_HASH);
+		baseKudo.setReviewerId(VALID_REVIEWER_HASH);
+		baseKudo.setComment(VALID_FEEDBACK_COMMENT);
+		baseKudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
+		baseKudo.setVersion(KUDO_CURRENT_VERSION);
+
+		return baseKudo;
+
+    }
+
     public Kudo assembleKudo(KudoOption kudoOption) {
 
-        Kudo kudo = new Kudo();
-        UserFactory userFactory = new UserFactory();
-        User validReviewer = userFactory.assembleUser(UserOption.VALID_ACTIVE_DEVELOPER);
-        validReviewer.setId(VALID_REVIEWER_HASH);
-        final String INVALID_COMMENT_LONG = generateRandomString(1501);
+        Kudo kudo = assembleBaseKudo();
+		final String INVALID_COMMENT_LONG = generateRandomString(1501);
 
         switch (kudoOption) {
 
             case VALID_KUDO:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
-                kudo.setVersion(KUDO_CURRENT_VERSION);
                 break;
 
             case INVALID_KUDO_REVIEWER_NULL:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
                 kudo.setReviewerId(null);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
-                kudo.setVersion(KUDO_CURRENT_VERSION);
                 break;
 
             case INVALID_KUDO_REVIEWED_NULL:
                 kudo.setReviewedId(null);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
-                kudo.setVersion(KUDO_CURRENT_VERSION);
                 break;
 
             case INVALID_KUDO_SELF_REVIEW:
                 kudo.setReviewedId(VALID_REVIEWER_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
                 break;
 
             case INVALID_KUDO_COMMENT_LONG:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
                 kudo.setComment(INVALID_COMMENT_LONG);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
-                kudo.setVersion(KUDO_CURRENT_VERSION);
                 break;
 
             case INVALID_KUDO_DATE_NULL:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
                 kudo.setSubmittedDate(null);
-                kudo.setVersion(KUDO_CURRENT_VERSION);
                 break;
 
             case INVALID_KUDO_DATE_PAST:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
                 kudo.setSubmittedDate(getDate(PAST_DATE));
-                kudo.setVersion(KUDO_CURRENT_VERSION);
 
             case INVALID_KUDO_VERSION_NULL:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
                 kudo.setVersion(null);
                 break;
 
             case INVALID_KUDO_VERSION_0:
-                kudo.setReviewedId(VALID_REVIEWED_HASH);
-                kudo.setReviewerId(VALID_REVIEWER_HASH);
-                kudo.setComment(VALID_FEEDBACK_COMMENT);
-                kudo.setSubmittedDate(new Date(System.currentTimeMillis() + DATE_OFFSET));
                 kudo.setVersion(0);
                 break;
         }
