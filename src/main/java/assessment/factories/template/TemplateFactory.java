970d87ddfd821d1a328c08c6ad3ee9658a651861
Joshua Tucker <jtucker@catalystitservices.com>
2016-05-03 17:48:16 -0700

    refactored Feedback and Template factories

--- src/main/java/assessment/factories/template/TemplateFactory.java
+++ src/main/java/assessment/factories/template/TemplateFactory.java
@@ -12,52 +12,50 @@ import java.util.List;
  */
 public class TemplateFactory extends TestBase {
 
-    public Template assembleAssessment(TemplateOption templateOption) {
+    public Template assembleBaseTemplate() {
 
-        Template template = new Template();
+		Template baseTemplate = new Template();
+
+		baseTemplate.setName(VALID_OBJECT_NAME);
+		baseTemplate.setQuestionList(QUESTION_LIST);
+		baseTemplate.setVersion(TEMPLATE_CURRENT_VERSION);
+
+		return baseTemplate;
+
+    }
+
+    public Template assembleTemplate(TemplateOption templateOption) {
+
+        Template template = assembleBaseTemplate();
         Template template2 = new Template();
+
         switch(templateOption){
 
             case VALID_TEMPLATE:
-                template.setName(VALID_OBJECT_NAME);
-                template.setQuestionList(QUESTION_LIST);
-                template.setVersion(TEMPLATE_CURRENT_VERSION);
                 break;
 
             case INVALID_TEMPLATE_DUPLICATE:
                 template.setName(TEMPLATE_NAME1);
-                template.setQuestionList(QUESTION_LIST);
-                template.setVersion(TEMPLATE_CURRENT_VERSION);
                 template2.setName(TEMPLATE_NAME2);
                 break;
 
             case INVALID_TEMPLATE_NAME_TOO_LONG:
                 template.setName(generateRandomString(102));
-                template.setQuestionList(QUESTION_LIST);
-                template.setVersion(TEMPLATE_CURRENT_VERSION);
                 break;
 
             case INVALID_TEMPLATE_NAME_SPECIAL_CHARACTERS:
                 template.setName(INVALID_OBJECT_NAME_SPECIAL_CHARACTERS);
-                template.setQuestionList(QUESTION_LIST);
-                template.setVersion(TEMPLATE_CURRENT_VERSION);
                 break;
 
             case INVALID_TEMPLATE_NULL_NAME:
                 template.setName(null);
-                template.setQuestionList(QUESTION_LIST);
-                template.setVersion(TEMPLATE_CURRENT_VERSION);
                 break;
 
             case INVALID_TEMPLATE_VERSION_ZERO:
-                template.setName(VALID_OBJECT_NAME);
-                template.setQuestionList(QUESTION_LIST);
                 template.setVersion(0);
                 break;
 
             case INVALID_TEMPLATE_VERSION_NULL:
-                template.setName(VALID_OBJECT_NAME);
-                template.setQuestionList(QUESTION_LIST);
                 template.setVersion(null);
                 break;
         }
@@ -65,12 +63,12 @@ public class TemplateFactory extends TestBase {
         return template;
     }
 
-    public List<Template> assembleAssessmentsAsList(TemplateOption option, int count) throws ParseException {
+    public List<Template> assembleTemplatesAsList(TemplateOption option, int count) throws ParseException {
 
         List<Template> result = new ArrayList<Template>();
 
         for (int i = 0; i < count; i++) {
-            result.add(assembleAssessment(option));
+            result.add(assembleTemplate(option));
         }
 
         return result;
